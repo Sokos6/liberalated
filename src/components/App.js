2df7c74728ec04c702477031c4b9857b04d70328
Sokos6 <will.sokolowski@gmail.com>
2020-11-14 20:11:19 -0600

    initial setup done

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import '../styles/App.css'
+import Header from './Header.js';
 
 function App() {
   return (
     <div className="App">
+    <Header />
      HERE WE GO LET'S GET FUNDED
     </div>
   );
