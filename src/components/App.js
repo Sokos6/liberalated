96a46040a988b24eccff8219306c692344ec2c3d
Sokos6 <will.sokolowski@gmail.com>
2020-11-14 20:29:34 -0600

    ready for routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 import { useState } from 'react';
-import '../styles/App.css'
+import '../styles/App.css';
 import Header from './Header.js';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { setContext } from 'apollo-link-context';
+
+// apollo client
+// Todo: extract this
+const httpLink = new HttpLink({
+  uri: 'https://modernliberal.hasura.app/v1/graphql',
+});
 
 function App() {
+  const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <div className="App">
-    <Header />
-     HERE WE GO LET'S GET FUNDED
-    </div>
+    <ApolloProvider client={client}>
+      <div className='App'>
+        <Header />
+        HERE WE GO LET'S GET FUNDED
+      </div>
+    </ApolloProvider>
   );
 }
 
